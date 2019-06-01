bd7e362de34f163906c9a57e79b2d7310ab053e4
fewensa <fewensa@protonmail.com>
2019-06-01 20:14:24 +0800

    path windows

--- src/path.rs
+++ src/path.rs
@@ -1,17 +1,18 @@
 use std::{env, fs, io};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-pub fn canonicalize_path<S: AsRef<Path>>(path: S) -> io::Result<String> {
-  let buf = fs::canonicalize(path)?;
-  Ok(buf.to_str().ok_or(io::ErrorKind::NotFound)?.to_string())
+//pub fn canonicalize_path<S: AsRef<Path>>(path: S) -> io::Result<String> {
+//  let buf = fs::canonicalize(path)?;
+//  println!("{:?}", buf);
+//  Ok(buf.to_str().ok_or(io::ErrorKind::NotFound)?.to_string())
+//}
+
+pub fn root_dir() -> PathBuf {
+  Path::new("./").canonicalize().expect("Can not get root dir")
 }
 
-pub fn root_dir() -> String {
-  canonicalize_path("./").expect("Can not get root dir")
-}
-
-pub fn out_dir() -> String {
-  env::var("OUT_DIR").expect("Can not get out dir")
+pub fn out_dir() -> PathBuf {
+  Path::new(&env::var("OUT_DIR").expect("Can not get out dir")[..]).canonicalize().expect("Can not get out dir")
 }
 
 
--- tests/test_fs.rs
+++ tests/test_fs.rs
@@ -2,9 +2,10 @@
 
 #[test]
 fn test_append() {
-  let file = &format!("{}/_tmp.txt", toolkit::path::root_dir())[..];
-  assert!(toolkit::fs::append(file, "A").is_ok());
-  assert!(toolkit::fs::append(file, "B").is_ok());
-  assert!(toolkit::fs::append(file, "C").is_ok());
+  let file = toolkit::path::root_dir().join("_tmp.txt");
+  println!("{:?}", file);
+  assert!(toolkit::fs::append(&file, "A").is_ok());
+  assert!(toolkit::fs::append(&file, "B").is_ok());
+  assert!(toolkit::fs::append(&file, "C").is_ok());
 }
 
